2996e5d32e00c1beeb844ba532758f6424d9be9c
zuc7 <162511099+zuc7@users.noreply.github.com>
2025-09-16 13:02:50 +0200

    Create archivio.cpp

---
+++ archivio.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Squadra{
+    private:
+        string idSquadra;
+        string nome;
+        char Lega;
+        int anno;
+        string indirizzoSede;
+        double budgetAnnuale;
+        int nAbbonati;
+
+    public:
+        Squadra ();
+        ~Squadra ();
+};
+class Calciatore {
+    private:
+        /* data */
+    public:
+        Calciatore();
+        ~Calciatore();
+};
+
+
+int main(){
+    cout<<"ciao";
+    return 0;
+}
